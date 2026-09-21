075f4aa5ebf791e9b3c2c0043bc112e095bc3c16
bzayn02 <nagarkotibezay@gmail.com>
2026-09-21 12:23:40 +0545

    twosumsorted

--- learning.js
+++ learning.js
@@ -717,3 +717,22 @@
 //    return false;
 // }
 // hasDuplicateFast([1, 2, 2, 3, 5]);
+
+function twoSumSorted(arr, target) {
+   let left = 0;
+   let right = arr.length - 1;
+   while (left < right) {
+      const sum = arr[left] + arr[right];
+      if (sum === target) {
+         return [left, right];
+      } else if (sum < target) {
+         left++;
+      } else {
+         right--;
+      }
+   }
+   return null;
+}
+twoSumSorted([2, 7, 11, 15], 9);
+
+// sortedness required
